7726980491a2bd12e50b4d6b95f7bf20071fdf9f
talmagejsharp <talmage2@icloud.com>
2023-09-02 18:13:00 -1000

    allow the server to listen on 0.0.0.0 hopefully accessible from the internet.

--- app.js
+++ app.js
@@ -120,7 +120,7 @@ app.post('/login', async (req, res) => {
 
 // Start the Express server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
