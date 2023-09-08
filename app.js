f864c87602774bca1a71d4d239f15358defeb48d
talmagesharp <talmagesharp321@gmail.com>
2023-09-07 15:44:28 -1000

    change port to listen on 3000

--- app.js
+++ app.js
@@ -119,7 +119,7 @@ app.post('/login', async (req, res) => {
 // ...
 
 // Start the Express server
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
