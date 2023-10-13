34e3390d1284bd1cfbd1efaab1d8bf78493713ca
talmagesharp <talmagesharp321@gmail.com>
2023-10-12 15:19:16 -1000

    Revert "fix console.log bug"
    This reverts commit de5f97e8c543c76349bf9dc7c653991573bc6d18.

--- app.js
+++ app.js
@@ -161,7 +161,7 @@ app.post('/login', async (req, res) => {
   const token = jwt.sign({ userId: user._id }, 'your-secret-key', {expiresIn: '1hr'});
   res.cookie('jwt', token, { httpOnly: true, secure: false, maxAge: 3600000 }); // maxAge is set to 1 hour here
   res.status(200).json({ message: 'Login successful' });
-  console.log("successfullly signed in and sent the jwt token " + token);
+  console.Log("successfullly signed in and sent the jwt token " + token);
 
 //  console.log('login successful!');
 });
